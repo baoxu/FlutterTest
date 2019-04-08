06063f86385f0a54783f6df05f3668f68d3387f6
baoxu <xu.bao@jetsbike.com>
2019-04-08 14:57:30 +0800

    第3步: 添加一个 有状态的部件（Stateful widget）

--- flutter_app_test/lib/main.dart
+++ flutter_app_test/lib/main.dart
@@ -6,7 +6,7 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
+//    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -15,9 +15,34 @@ class MyApp extends StatelessWidget {
         ),
         body: new Center(
 //          child: new Text('Hello World'),
-          child: new Text(wordPair.asPascalCase),
-      ),
+//          child: new Text(wordPair.asPascalCase),
+          child: new RandomWords(),
+        ),
       ),
     );
+  }
+}
+
+
+/*
+* 添加有状态的 RandomWords widget 到 main.dart。
+* 它也可以在MyApp之外的文件的任何位置使用，但是本示例将它放到了文件的底部。
+* RandomWords widget除了创建State类之外几乎没有其他任何东西
+* */
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+/*
+添加 RandomWordsState 类.该应用程序的大部分代码都在该类中， 该类持有RandomWords widget的状态。
+这个类将保存随着用户滚动而无限增长的生成的单词对，
+以及喜欢的单词对，用户通过重复点击心形 ❤️ 图标来将它们从列表中添加或删除。
+* */
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
